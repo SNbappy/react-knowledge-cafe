568ec2598c520dcedf8a8e4150c0eec0ade73bce
SNbappy <bappy.just@gmail.com>
2024-10-28 18:27:20 +0600

    displayed blog data and hashtags

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Blog from "../Blog/Blog";
+import PropTypes from 'prop-types';
 
 const Blogs = () => {
     const [blogs, setBlogs] = useState([]);
@@ -13,6 +15,9 @@ const Blogs = () => {
     return (
         <div className="md:w-2/3">
             <h1 className="text-4xl">Blogs: {blogs.length}</h1>
+            {
+                blogs.map(blog => <Blog key={blog.id} blog={blog}></Blog>)
+            }
         </div>
     );
 };
