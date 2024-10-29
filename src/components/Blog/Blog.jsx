8ff15639a61b862821620e7052008e65d897b3f5
SNbappy <bappy.just@gmail.com>
2024-10-29 06:42:26 +0600

    added bookmark button

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,12 +1,19 @@
-import { space } from 'postcss/lib/list';
 import PropTypes from 'prop-types'
+import { FaBookmark } from 'react-icons/fa';
 
 const Blog = ({ blog }) => {
     const { title, cover, author, author_img, posted_date, reading_time, hashtags } = blog;
+
+    // Define the handleAddToBookmark function
+    const handleAddToBookmark = () => {
+        console.log(`${title} added to bookmarks!`);
+        // Add any other bookmark functionality here
+    };
+
     return (
-        <div>
-            <img src={cover} alt={`Cover picture of the title ${title}`} />
-            <div className='flex justify-between'>
+        <div className='mb-20'>
+            <img className='w-full mb-8' src={cover} alt={`Cover picture of the title ${title}`} />
+            <div className='flex justify-between mb-4'>
                 <div className='flex'>
                     <img className='w-14' src={author_img} alt="" />
                     <div className='ml-6'>
@@ -16,6 +23,9 @@ const Blog = ({ blog }) => {
                 </div>
                 <div>
                     <span>{reading_time} min read</span>
+                    <button
+                        onClick={handleAddToBookmark}
+                        className='ml-2 text-2xl text-red-600'><FaBookmark></FaBookmark></button>
                 </div>
             </div>
             <h2 className="text-4xl">{title}</h2>
